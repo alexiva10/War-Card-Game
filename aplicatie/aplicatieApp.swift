70cfbed5f7ed128496aa3c735aaf0033baa23cea
Alexandru Ivanescu <alexbogdnivanescu@gmai.com>
2022-09-03 13:01:59 +0300

    Initial Commit

---
+++ aplicatie/aplicatieApp.swift
@@ -0,0 +1,17 @@
+//
+//  aplicatieApp.swift
+//  aplicatie
+//
+//  Created by Alex Ivanescu on 14.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct aplicatieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
